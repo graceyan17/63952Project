f845b4700af6909664a8eb28cc21288d8ad027f7
Grace Yan <graceyan@mit.edu>
2025-11-30 17:34:54 -0500

    Edit download datasets code

--- download_datasets.py
+++ download_datasets.py
@@ -1,17 +1,26 @@
+import os
 import kagglehub
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
 
+dir1 = "datasets/goodreads-book-information-descriptions"
+dir2 = "datasets/goodreadsbooks-book-information-pgct"
+dir3 = "datasets/book-recommendation-user-reviews"
+
+os.makedirs(dir1, exist_ok=True)
+os.makedirs(dir2, exist_ok=True)
+os.makedirs(dir3, exist_ok=True)
+
 # goodreads-book-information-descriptions
-path = kagglehub.dataset_download("rohitganeshkar/goodreads-book-recommendation-datasets")
+path = kagglehub.dataset_download("rohitganeshkar/goodreads-book-recommendation-datasets", path=dir1)
 print("Path to dataset files:", path)
 
 # goodreadsbooks-book-information-pgct
-path = kagglehub.dataset_download("jealousleopard/goodreadsbooks")
+path = kagglehub.dataset_download("jealousleopard/goodreadsbooks", path=dir2)
 print("Path to dataset files:", path)
 
 # book-recommendation-user-reviews
-path = kagglehub.dataset_download("arashnic/book-recommendation-dataset")
+path = kagglehub.dataset_download("arashnic/book-recommendation-dataset", path=dir3)
 print("Path to dataset files:", path)
